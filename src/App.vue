5cc86d1b2bbd2ac7020331bc552597bf26ab3aa9
Gustavo Morales <gustavo.morales@gmail.com>
2018-01-27 21:49:15 +0000

    Add form to add Book

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
-    <div>
+    <form id="form">
       <p>
-        <label>Title: <input type="text" v-model="book.title"></label>
+        <label>Title: <input type="text" name="title" v-model="book.title"></label>
       </p>
       <p>
-        <label>Description: <textarea v-model="book.description"></textarea></label>
+        <label>Description: <textarea name="description" v-model="book.description"></textarea></label>
       </p>
       <p>
-        <label>Author: <input type="text" v-model="book.author"></label>
+        <label>Author: 
+        <select name="author" v-model="book.author">
+          <option v-for="author in authors" :value="author._id">
+            {{ author.firstname }} {{ author.lastname }}
+          </option>
+        </select>
+        </label>
       </p>
-      <button @click="create">Create</button>
-    </div>
+      <button @click.prevent="create">Create</button>
+    </form>
     <hr />
     <p v-if="loading">Loading ...</p>
     <ul v-else>
@@ -32,6 +38,7 @@ export default {
     return {
       loading: true,
       books: [],
+      authors: [],
       book: {
         title: '',
         description: '',
@@ -40,24 +47,42 @@ export default {
     }
   },
   created (){
-    axios.get(`${config.baseURL}/books`,{
-      withCredentials: true
-    })
-    .then( response => {
-      this.loading = false;
-      this.books = response.data.data;
-    })
+    this._loadBooks();
+    this._loadAuthors();
   },
   methods: {
-    create(){
-      axios({
-        method: 'post',
-        url: `${config.baseURL}/books`,
-        data: this.book,
+    _loadBooks(){
+      axios.get(`${config.baseURL}/books`,{
         withCredentials: true
       })
       .then( response => {
-        this.books.shift(response.data.data);
+        this.loading = false;
+        this.books = response.data.data;
+      })
+    },
+    _loadAuthors(){
+      axios.get(`${config.baseURL}/authors`,{
+        withCredentials: true
+      })
+      .then( response => {
+        this.authors = response.data.data;
+      })
+    },
+    create(){
+      
+      let payload = "";
+      Object.keys(this.book).forEach(key => {
+        payload+=`${key}=${this.book[key]}&`
+      });
+      
+      axios.post(`${config.baseURL}/books`, payload, {
+        withCredentials: true,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      })
+      .then( response => {
+        this._loadBooks();
       })
     }
   }
