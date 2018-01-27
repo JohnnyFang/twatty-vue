a37cbb10726a1c97cc610e9030db39611da10d03
Gustavo Morales <gustavo.morales@gmail.com>
2018-01-27 16:56:00 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="book in books">
+        {{ book.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      books: [{
+        _id: "1",
+        title: "The power of habits"
+      },{
+        _id: "2",
+        title: "Steal the show"
+      }]
+    }
+  }
+}
+</script>
