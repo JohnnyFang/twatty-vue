1c47a10f2ff235a981bbbe7f515e4620dbac69fc
Gustavo Morales <gustavo.morales@gmail.com>
2018-01-27 19:55:19 +0000

    Add book

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div id="app">
+    <div>
+      <p>
+        <label>Title: <input type="text" v-model="book.title"></label>
+      </p>
+      <p>
+        <label>Description: <textarea v-model="book.description"></textarea></label>
+      </p>
+      <p>
+        <label>Author: <input type="text" v-model="book.author"></label>
+      </p>
+      <button @click="create">Create</button>
+    </div>
+    <hr />
     <p v-if="loading">Loading ...</p>
     <ul v-else>
-      <li v-for="book in books">
-        {{ book.title }}
+      <li v-for="book in books" :key="book._id">
+        {{ book.title }} - {{ book.author.firstname }} {{ book.author.lastname }}
       </li>
     </ul>
   </div>
@@ -18,17 +31,35 @@ export default {
   data () {
     return {
       loading: true,
-      books: []
+      books: [],
+      book: {
+        title: '',
+        description: '',
+        author: ''
+      }
     }
   },
   created (){
     axios.get(`${config.baseURL}/books`,{
       withCredentials: true
     })
-      .then( response => {
-        this.loading = false;
-        this.books = response.data.data;
+    .then( response => {
+      this.loading = false;
+      this.books = response.data.data;
+    })
+  },
+  methods: {
+    create(){
+      axios({
+        method: 'post',
+        url: `${config.baseURL}/books`,
+        data: this.book,
+        withCredentials: true
       })
+      .then( response => {
+        this.books.shift(response.data.data);
+      })
+    }
   }
 }
 </script>
